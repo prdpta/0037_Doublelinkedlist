d750d69972131645ed75fea15a8c0e26cdd01871
prdpta <pradiptaputramei01@gmail.com>
2024-05-04 18:50:43 +0700

    menambahkan if dan delete

--- 0037_Doublelinkedlist.cpp
+++ 0037_Doublelinkedlist.cpp
@@ -121,3 +121,13 @@ bool search(int rollNo, Node** previous, Node** current)
         else
         {
             previous->next = current->next;
+            if (current->next != NULL)
+            {
+                current->next->prev = previous;
+            }
+        }
+
+        delete current;
+        cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\1b[0m" << endl;
+
+    }
